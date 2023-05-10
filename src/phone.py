18043d8fd728d487257560c290340da14ac6be1b
Evelinabas0612 <evelinabas0612@gmail.com>
2023-05-10 21:59:16 +0300

    changed phone.py

--- src/phone.py
+++ src/phone.py
@@ -4,17 +4,24 @@ from src.item import Item
 class Phone(Item):
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
         super().__init__(name, price, quantity)
-        if number_of_sim > 0:
-            self.number_of_sim = number_of_sim
+        self.__number_of_sim = number_of_sim
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, new_number):
+        if new_number > 0:
+            self.__number_of_sim = new_number
         else:
-            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
-        Phone.all.append(self)
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
 
     def __str__(self):
         return self.name
 
     def __repr__(self):
-        return f'Phone(\'{self.name}\', {self.price}, {self.quantity}, {self.number_of_sim})'
+        return f'Phone(\'{self.name}\', {self.price}, {self.quantity}, {self.__number_of_sim})'
 
     def __add__(self, other):
         """
